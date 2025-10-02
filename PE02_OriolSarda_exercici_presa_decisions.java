79f4057cd9db030d3103d67e8100aedfc2ed5653
Oriol Sarda <alu.oriol.sarda@mataro.epiaedu.cat>
2025-10-02 16:04:17 +0200

    Versio 1.0

---
+++ PE02_OriolSarda_exercici_presa_decisions.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class PE02_OriolSarda_exercici_presa_decisions {
+    public static void main(String[] args) {
+        Scanner e = new Scanner(System.in);
+
+    }
+}
